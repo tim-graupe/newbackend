a9924eccb81cd3814682b320f55d24bed9e5516f
tim-graupe <tgraupe90@gmail.com>
2024-02-11 09:17:02 -0500

    define cors

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const cors = require("cors");
+
 const passport = require("passport");
 const bcrypt = require("bcrypt");
 const User = require("./models/newUserModel");
